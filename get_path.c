60f3a3485630066839cb502796451708c851403c
Abdelaziz_Elhemaily <abdelazizelhemaily@gmail.com>
2023-05-23 02:16:45 +0300

    5.1

---
+++ get_path.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * get_path - get path from environment
+ * @env: environment variables
+ * Return: PATH value
+ */
+
+char *get_path(char **env)
+{
+	int len = 0;
+	char *Path = NULL, *full_path = NULL;
+
+	Path = getenv("PATH");
+	if (!Path)
+		return (NULL);
+	len = _strlen(Path);
+	full_path = malloc(sizeof(char) * (len + 1));
+	if (!full_path)
+		return (NULL);
+	full_path = _strcpy(full_path, Path);
+	return (full_path);
+	(void)env;
+}
